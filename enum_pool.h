2f2e0d5aa5c3a3f58b5114448939e3010a123d49
sajkop123 <none>
2023-08-12 17:58:13 +0800

    [type]: impl enum_map
    [Detail]
    basic interface & impl is done.
    
    [Scope]

--- enum_pool.h
+++ enum_pool.h
@@ -2,6 +2,7 @@
 #include <map>
 #include <array>
 #include <stdexcept>
+#include <algorithm>
 
 #include "common.h"
 
@@ -12,7 +13,7 @@
 
 template<typename _Key,
          typename _Tp,
-         size_t _MaxEnumSize = 10>
+         size_t _MaxEnumSize = static_cast<size_t>(_Key::ENUM_MAP_MAX_SIZE)>
 class enum_map {
  private:
   typedef std::map<_Key, _Tp> stlmap_container_t;
@@ -22,73 +23,75 @@ class enum_map {
   typedef _Key                                          key_type;
   typedef _Tp                                           mapped_type;
   typedef std::pair<const _Key, _Tp>                    value_type;
-  typedef typename stlmap_container_t::reference        reference;
-  typedef typename stlmap_container_t::const_reference  const_reference;
-  typedef typename stlmap_container_t::iterator         iterator;
-  typedef typename stlmap_container_t::const_iterator   const_iterator;
-  typedef typename stlmap_container_t::pointer          pointer;
-  typedef typename stlmap_container_t::const_pointer    const_pointer;
-  typedef typename stlmap_container_t::size_type        size_type;
+  typedef std::list<value_type>                         data_container_t;
+  typedef typename data_container_t::reference          reference;
+  typedef typename data_container_t::const_reference    const_reference;
+  typedef typename data_container_t::iterator           iterator;
+  typedef typename data_container_t::const_iterator     const_iterator;
+  typedef typename data_container_t::pointer            pointer;
+  typedef typename data_container_t::const_pointer      const_pointer;
+  typedef typename data_container_t::size_type          size_type;
   typedef std::reverse_iterator<iterator>               reverse_iterator;
   typedef std::reverse_iterator<const_iterator>         const_reverse_iterator;
 
- private:
-  typedef std::list<value_type> data_container_t;
   typedef std::array<pointer, _MaxEnumSize> mapping_container_t;
 
- public:
-  explicit enum_map() : m_ptr({nullptr}) {}
 
-  enum_map(std::initializer_list<value_type> __l)
-      : enum_map() {
+ public:  // Member functions:
+  explicit enum_map() {
+    clear();
+  }
+
+  enum_map(std::initializer_list<value_type> __l) {
+    __clear_m_data();
     for (auto& it : __l) {
       m_data.emplace_back(it);
     }
-    fill_m_ptr();
+    __fill_m_ptr();
   }
 
   enum_map(const enum_map& __other) {
     m_data = __other.m_data;
-    fill_m_ptr();
+    __fill_m_ptr();
   }
 
   enum_map(enum_map&& __other) {
     m_data = std::forward<data_container_t>(__other.m_data);
-    fill_m_ptr();
+    __fill_m_ptr();
     __other.clear();
   }
 
   template<typename _InputIterator>
   enum_map(_InputIterator __first, _InputIterator __second) {
+    clear();
     for (auto& it = __first; it != __second; ++it) {
       m_data.emplace_back(it);
     }
-    fill_m_ptr();
+    __fill_m_ptr();
   }
 
   ~enum_map() = default;
 
-  // Member functions:
-
   enum_map& operator=(const enum_map& __other) {
     m_data = __other.m_data;
-    fill_m_ptr();
+    __fill_m_ptr();
     return *this;
-  }
-  enum_map& operator=(enum_map&& __other) {
-    m_data = std::forward<data_container_t>(__other.m_data);
-    fill_m_ptr();
-    __other.clear();
-    return *this;
-  }
-  enum_map& operator=(std::initializer_list<value_type> __l) {
-    for (auto& it : __l) {
-      m_data.emplace_back(it);
-    }
-    fill_m_ptr();
   }
 
-  // Element access:
+  enum_map& operator=(enum_map&& __other) {
+    m_data = std::move<data_container_t>(__other.m_data);
+    m_ptr = std::move<data_container_t>(__other.m_ptr);
+    return *this;
+  }
+
+  enum_map& operator=(std::initializer_list<value_type> __l) {
+    clear();
+    for (auto& it : __l) {
+      __insert_unique_(it);
+    }
+  }
+
+ public:  // Element access:
   mapped_type&
   at(const key_type& __key) {
     pointer p = m_ptr[index(__key)];
@@ -107,9 +110,82 @@ class enum_map {
     return (*p).second;
   }
 
-  // Iterators:
+  mapped_type&
+  operator[](const key_type& __key) {
+    pointer p = m_ptr[index(__key)];
+    if (!p) {
+      // need make sure mapped_type is default constructible
+      m_data.emplace_back(value_type(__key, mapped_type()));
+      p = std::addressof(m_data.back());
+      m_ptr[index(__key)] = p;
+    }
+    return *p;
+  }
 
-  // Capacity:
+  mapped_type&
+  operator[](key_type&& __key) {
+    pointer p = m_ptr[index(std::forward<key_type>(__key))];
+    if (!p) {
+      // need make sure mapped_type is default constructible
+      m_data.emplace_back(value_type(__key, mapped_type()));
+      p = std::addressof(m_data.back());
+      m_ptr[index(__key)] = p;
+    }
+    return *p;
+  }
+
+ public:  // Iterators:
+  iterator
+  begin() {
+    return m_data.begin();
+  }
+
+  iterator
+  end() {
+    return m_data.end();
+  }
+
+  const_iterator
+  begin() const noexcept {
+    return m_data.begin();
+  }
+
+  const_iterator
+  end() const noexcept {
+    return m_data.end();
+  }
+
+  const_iterator
+  cbegin() const noexcept {
+    return m_data.cbegin();
+  }
+
+  const_iterator
+  cend() const noexcept {
+    return m_data.cend();
+  }
+
+  reverse_iterator
+  rbegin() noexcept {
+    return m_data.rbegin();
+  }
+
+  reverse_iterator
+  rend() noexcept {
+    return m_data.rend();
+  }
+
+  const_reverse_iterator
+  crbegin() const noexcept {
+    return m_data.rbegin();
+  }
+
+  const_reverse_iterator
+  crend() const noexcept {
+    return m_data.rend();
+  }
+
+ public:  // Capacity:
   bool
   empty() const noexcept {
     return size();
@@ -125,23 +201,96 @@ class enum_map {
     return _MaxEnumSize;
   }
 
-  // Modifier:
+ public:  // Modifier:
   void
   clear() noexcept {
-    m_data.clear();
-    std::fill(m_ptr.begin(), m_ptr.end(), nullptr);
+    __clear_m_data();
+    __clear_m_ptr();
   }
 
-  // std::pair<iterator, bool>
-  // insert(const value_type& __x) {
+  std::pair<iterator, bool>
+  insert(const value_type& __value) {
+    return __insert_unique_(__value);
+  }
 
+  std::pair<iterator, bool>
+  insert(value_type&& __value) {
+    return __insert_unique_(std::move(__value));
+  }
+
+  void
+  insert(std::initializer_list<value_type> __l) {
+    for (auto& it : __l) {
+      insert(it);
+    }
+  }
+
+  template<class... Args>
+  std::pair<iterator, bool>
+  emplace(Args&&... __args) {
+    return __insert_unique_(std::forward<Args>(__args)...);
+  }
+
+  // template <typename... _Args>
+	// std::pair<iterator, bool>
+	// try_emplace(const key_type& __key, _Args&&... __args) {
+  //   insert
   // }
 
 
+  void
+  erase(iterator __pos) {
+    __erase_by_iter_(__pos);
+  }
 
-  // Lookup:
+  void erase(const_iterator __pos) {
+    __erase_by_iter_(__pos);
+  }
 
- private:
+  size_type
+  erase(const key_type& __key) {
+    return __erase_by_key_(__key);
+  }
+
+  size_type
+  erase(key_type&& __key) {
+    return __erase_by_key_(std::forward<key_type>(__key));
+  }
+
+  void swap(enum_map& other) {
+    std::swap(m_data, other.m_data);
+    std::swap(m_ptr, other.m_ptr);
+  }
+
+ public:  // Lookup:
+  size_type
+  count(const key_type& __key) {
+    return m_ptr[index(__key)] ? 1 : 0;
+  }
+
+  size_type
+  count(key_type&& __key) {
+    return m_ptr[index(std::move(__key))] ? 1 : 0;
+  }
+
+  iterator
+  find(const key_type& __key) const {
+    return std::find(m_data.begin(), m_data.end(),
+          [&](const value_type& v) {
+            return v.first == __key;
+          });
+  }
+
+  const_iterator
+  find(const key_type& __key) {
+    return std::find(m_data.begin(), m_data.end(),
+          [&](const value_type& v) {
+            return v.first == __key;
+          });
+  }
+
+ private:  // private method
+
   inline size_t
   index(const key_type& __key) const {
     size_t index = static_cast<size_t>(__key);
@@ -151,10 +300,120 @@ class enum_map {
     return index;
   }
 
-  inline void fill_m_ptr() {
+  inline size_t
+  index(key_type&& __key) const {
+    size_t index = static_cast<size_t>(std::forward<key_type>(__key));
+    if (index < 0 || index >= _MaxEnumSize) {
+      throw std::runtime_error("invalid enum to size_t");
+    }
+    return index;
+  }
+
+  /**
+   * make insertion of a value_type object.
+   *
+   * @param __value
+   * @return std::pair<iterator, bool>
+   *    consisting of an iterator to the inserted element and a bool value
+   *    set to `true` if and only if the insertion took place.
+   */
+  inline std::pair<iterator, bool>
+  __insert_unique_(value_type&& __value) {
+    size_t id = index(std::forward<value_type>(__value).first);
+    pointer p = m_ptr[id];
+    if (!p) {
+      // not stored, push a new value_type data
+      m_data.emplace_back(std::forward<value_type>(__value));
+      p = std::addressof(m_data.back());
+      m_ptr[id] = p;
+      return {std::prev(m_data.end()), false};
+    } else {
+      // already stored, overwrite the mapped_type data
+      // not sure if there any solution better than search...
+      iterator it = std::find_if(m_data.begin(), m_data.end(),
+          [__key = __value.first](const value_type& v) {
+            return v.first == __key;
+          });
+      if (it != m_data.end()) {
+        (*it).second = std::forward<value_type>(__value).second;
+      }
+      return {it, true};
+    }
+  }
+
+  /**
+   * insert in-place if the key does not exist, do nothing if the key exists.
+   *
+   * @param __value same as for emplace
+   * @return std::pair<iterator, bool> same as for emplace
+   */
+  inline std::pair<iterator, bool>
+  __try_insert_unique_(value_type&& __value) {
+    size_t id = index(std::forward<value_type>(__value).first);
+    pointer p = m_ptr[id];
+    if (!p) {
+      // not stored, push a new value_type data
+      m_data.emplace_back(std::forward<value_type>(__value));
+      p = std::addressof(m_data.back());
+      m_ptr[id] = p;
+      return {std::prev(m_data.end()), false};
+    } else {
+      // already stored, search the iterator only, do nothing.
+      // not sure if there any solution better than search...
+      iterator it = std::find_if(m_data.begin(), m_data.end(),
+          [__key = __value.first](const value_type& v) {
+            return v.first == __key;
+          });
+      return {it, true};
+    }
+  }
+
+  inline size_type
+  __erase_by_key_(key_type&& __key) {
+    iterator it = std::find_if(m_data.begin(), m_data.end(),
+        [&](const value_type& v) {
+          return v.first == __key;
+        });
+    if (it != m_data.end()) {
+      m_data.erase(it);
+      __fill_m_ptr();
+      return 1;
+    }
+    return 0;
+  }
+
+  inline size_type
+  __erase_by_key_(const key_type& __key) {
+    iterator it = std::find_if(m_data.begin(), m_data.end(),
+        [&](const value_type& v) {
+          return v.first == __key;
+        });
+    if (it != m_data.end()) {
+      m_data.erase(it);
+      __fill_m_ptr();
+      return 1;
+    }
+    return 0;
+  }
+
+  inline void
+  __erase_by_iter_(const_iterator __pos) {
+    m_data.erase(__pos);
+    __fill_m_ptr();
+  }
+
+  inline void __fill_m_ptr() {
+    __clear_m_ptr();
     for (auto& data : m_data) {
       m_ptr[index(data.first)] = std::addressof(data);
     }
+  }
+
+  inline void __clear_m_data() {
+    m_data.clear();
+  }
+  inline void __clear_m_ptr() {
+    std::fill(m_ptr.begin(), m_ptr.end(), nullptr);
   }
 
  private:
--- test_main.cpp
+++ test_main.cpp
@@ -1,16 +1,86 @@
 #include "enum_pool.h"
 
+#include <sstream>
+
 enum ENUM {
   A,
   B,
   C,
   D,
+  ENUM_MAP_MAX_SIZE,
 };
 
-int main() {
-  std::map<int, int> b{{1,1}, {2,2}};
-  enum_map<ENUM, int> a{{ENUM::B,10}, {ENUM::C,2}};
+inline const char* to_str(ENUM e) {
+  switch(e) {
+    case A: return "A";
+    case B: return "B";
+    case C: return "C";
+    case D: return "D";
+    default : return "UNKNOWN";
+  }
+};
 
-  const int c = a.at(ENUM::B);
-  MY_LOGD("%d", c);
+static inline void print_enum_map(const enum_map<ENUM, int>& map) {
+  std::ostringstream log;
+  for (const auto it : map) {
+    log << "(" << to_str(it.first) << ", " << it.second << ")";
+  }
+  MY_LOGD("%s", log.str().c_str());
 }
+
+int main() {
+  enum_map<ENUM, int> map{{ENUM::B,10}, {ENUM::C,2}};
+  print_enum_map(map);
+
+  {
+    const int c = map.at(ENUM::B);
+    print_enum_map(map);
+  }
+
+  {
+    MY_LOGD("1. add ENUM::A, 99");
+    map.insert({ENUM::A, 99});
+    const int c = map.at(ENUM::A);
+    print_enum_map(map);
+  }
+  {
+    MY_LOGD("2. insert ENUM::D, 654");
+    map.insert({ENUM::D, 654});
+    const int c = map.at(ENUM::B);
+    print_enum_map(map);
+  }
+  {
+    MY_LOGD("3. insert ENUM::B, 8");
+    int value = 8;
+    map.insert({ENUM::B, value});
+    const int c = map.at(ENUM::B);
+    print_enum_map(map);
+  }
+  {
+    MY_LOGD("4. emplace pair(ENUM::D, 77777)");
+    map.emplace(std::make_pair(ENUM::D, 77777));
+    const int c = map.at(ENUM::B);
+    print_enum_map(map);
+  }
+  {
+    MY_LOGD("5. erase ENUM::C");
+    for (auto it = map.begin(); it != map.end();) {
+      if (it->first == ENUM::C) {
+        map.erase(it);
+        break;
+      } else {
+        it++;
+      }
+    }
+    print_enum_map(map);
+  }
+  {
+    MY_LOGD("6. swap with another enum_map({C, 777}, {D, 7777})");
+    enum_map<ENUM, int> new_a{{ENUM::C,777}, {ENUM::D,7777}};
+    map.swap(new_a);
+    print_enum_map(map);
+  }
+}
+
+
+// https://quick-bench.com/q/h2ZlNfzMFBuhe-A0yjgg2VuMv4g
